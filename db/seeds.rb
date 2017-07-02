eacb12807cf64c33a1eec1499a96c06fdd7b4a7a
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-07-02 13:44:26 -0700

    faker seed database products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  product = Product.create!(name: Faker::Commerce.product_name,
+                            image: Faker::Placeholdit.image ,
+                            cost: Faker::Commerce.price,
+                            description: Faker::Commerce.department,
+                            country_origin: Faker::Address.country)
+  # 5.times do |index|
+  #   product.reviews.create!(author: Faker::Name.unique.name,
+  #                           content: Faker::HitchhikersGuideToTheGalaxy.quote,
+  #                           rating: Faker::Number.between(1, 5))
+  # end
+end
+
+
+p "Created #{Product.count} products"
+# p "Created #{Review.count} reviews"
