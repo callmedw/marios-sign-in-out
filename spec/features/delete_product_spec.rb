13be3c1246d36f8e7e7ee47e316e1cd582f85a0d
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-07-02 21:21:54 -0700

    on integration specs

---
+++ spec/features/delete_product_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the delete a product pathway" do
+  it "deletes an existing product" do
+    visit products_path
+    click_link 'Add a New Product'
+    fill_in 'product[name]', :with => 'Craftsmen Workbench'
+    fill_in 'product[cost]', :with => '500.00'
+    fill_in 'product[country_origin]', :with => 'United States'
+    fill_in 'product[description]', :with => 'Crafts, man!'
+    fill_in 'product[image]', :with => 'x'
+    click_on 'Create Product'
+    expect(page).to have_content 'Craftsmen Workbench'
+    click_on page.all("Craftsmen Workbench").first()
+
+    click_on 'Remove this product'
+    expect(page).to have_content "Mario's"
+  end
+end
