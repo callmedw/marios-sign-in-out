9bf5e43a9d2e38a3bf9f5eaeb7f08c90a55afee6
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-07-02 17:58:22 -0700

    scopes: alpha, recent, most reviewed, local

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,16 @@ class Product < ActiveRecord::Base
   validates :cost, :presence => true
   validates :description, :presence => true
   validates :country_origin, :presence => true
+
+  scope :creationNew, -> { order(created_at: :desc).limit(5)}
+  scope :alpha, -> { order(name: :asc)}
+  scope :mostReviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+      .joins(:reviews)
+      .group("products.id")
+      .order("reviews_count DESC")
+      .limit(5)
+    )}
+  scope :local, -> { where(country_origin: "United States")}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
+Review.destroy_all
 
-50.times do |index|
+40.times do |index|
   product = Product.create!(name: Faker::Commerce.product_name,
                             image: Faker::Placeholdit.image ,
                             cost: Faker::Commerce.price,
                             description: Faker::Commerce.department,
                             rating: Faker::Number.between(1, 5),
                             country_origin: Faker::Address.country)
+  5.times do |index|
+    product.reviews.create!(author: Faker::Name.unique.name,
+                            content: Faker::TwinPeaks.quote)
+  end
+end
+
+10.times do |index|
+  product = Product.create!(name: Faker::Commerce.product_name,
+                            image: Faker::Placeholdit.image ,
+                            cost: Faker::Commerce.price,
+                            description: Faker::Commerce.department,
+                            rating: Faker::Number.between(1, 5),
+                            country_origin: 'United States')
   5.times do |index|
     product.reviews.create!(author: Faker::Name.unique.name,
                             content: Faker::TwinPeaks.quote)
